07ed584d073af2328746f49c8f88115e548899ab
ENZO332 <e.gustavojuarez@gmail.com>
2025-10-03 14:29:17 -0300

    feat: crear el proyecto spring

---
+++ ingsw-app/src/main/java/com/grupo1/ingsw_app/IngswAppApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.ingsw_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngswAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngswAppApplication.class, args);
+	}
+
+}
---
+++ ingsw-app/src/test/java/com/grupo1/ingsw_app/IngswAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.ingsw_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngswAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
